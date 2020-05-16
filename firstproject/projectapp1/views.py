6bcc72f69b9825160bf85839e7deb038a535195d
raj kumar bheemreddy <rajthedev28@gmail.com>
2020-05-16 18:26:47 +1000

    changed comment

--- firstproject/projectapp1/views.py
+++ firstproject/projectapp1/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here
 def hello_world_view(request):
     return HttpResponse('<h1>This is simple Django Application</h1>')
